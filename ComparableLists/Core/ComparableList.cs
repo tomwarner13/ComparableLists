e03a005e1005307204f11b368653ce2d7b7ce988
Tom Warner <twarner@dealeron.com>
2016-08-23 09:33:11 -0400

    refactor everything

--- ComparableLists/Core/ComparableList.cs
+++ ComparableLists/Core/ComparableList.cs
@@ -6,67 +6,52 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-	//fair warning: this comparison is probably grotesquely inefficient, and will sort both lists for you so if you don't want that, sucks to suck
-	public class ComparableList<T> : List<T> where T : IComparable
-	{
-		public static ComparableList<T> Of(IEnumerable<T> source)
-		{
-			var list = new ComparableList<T>();
+  /// <summary>
+  /// A list which is equatable to another list based on the equality of it's items, regardless of order. 
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class ComparableList<T> : List<T>, IEquatable<ComparableList<T>> where T : IComparable, IEquatable<T>
+  {
+    public static ComparableList<T> Of(IEnumerable<T> source)
+    {
+      var list = new ComparableList<T>();
+      list.AddRange(source);
+      return list;
+    }
 
-			foreach (var item in source)
-			{
-				list.Add(item);
-			}
+    public static ComparableList<T> Of(params T[] source)
+    {
+      return Of(source as IEnumerable<T>);
+    }
 
-			return list;
-		}
+    public bool Equals(ComparableList<T> other)
+    {
+      return other?.OrderBy(t => t).SequenceEqual(this.OrderBy(t => t)) ?? false;
+    }
 
-		public static ComparableList<T> Of(params T[] source)
-		{
-			return Of(source as IEnumerable<T>);
-		}
+    public override bool Equals(object obj)
+    {
+      return Equals(obj as ComparableList<T>);
+    }
 
-		public override bool Equals(object obj)
-		{
-			if (obj == null)
-			{
-				return false;
-			}
+    public override int GetHashCode()
+    {
+      unchecked
+      {
+        int hashCode = 0;
+        foreach (var item in this)
+        {
+          hashCode = (hashCode * 397) ^ (item?.GetHashCode() ?? 0);
+        }
+        return hashCode;
+      }
+    }
 
-			List<T> other = obj as List<T>;
-			if (other == null)
-			{
-				return false;
-			}
+    //
+    // Equality Operators
+    //
 
-			other.Sort();
-			Sort();
-
-			int i = 0;
-			foreach (var item in this)
-			{
-				if (Comparer<T>.Default.Compare(item, other[i]) == 0)
-				{
-					return false;
-				}
-				i++;
-			}
-
-			return true;
-		}
-
-
-		public override int GetHashCode()
-		{
-			unchecked
-			{
-				int hashCode = 0;
-				foreach (var item in this)
-				{
-					hashCode = (hashCode * 397) ^ (item?.GetHashCode() ?? 0);
-				}
-				return hashCode;
-			}
-		}
-	}
+    public static bool operator ==(ComparableList<T> a, ComparableList<T> b) => a?.Equals(b) ?? Object.ReferenceEquals(b, null);
+    public static bool operator !=(ComparableList<T> a, ComparableList<T> b) => !(a == b);
+  }
 }
