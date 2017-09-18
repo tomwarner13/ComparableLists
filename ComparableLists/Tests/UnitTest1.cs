75cd7d8b8b5ccc8b1b024baae8216d9f5ddc2fea
Tom Warner <twarner@aps-dom.apsfinancial.com>
2017-09-18 17:17:24 -0600

    add unit tests

---
+++ ComparableLists/Tests/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBasicEquality()
+        {
+            var l1 = new ComparableList<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            var l2 = new ComparableList<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            Assert.AreEqual(l1, l2);
+        }
+
+        [TestMethod]
+        public void TestUnsortedEquality()
+        {
+            var l1 = new ComparableList<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            var l2 = new ComparableList<string>()
+            {
+                "a",
+                "c",
+                "b"
+            };
+
+            Assert.AreEqual(l1, l2);
+        }
+
+        [TestMethod]
+        public void TestInequality()
+        {
+            var l1 = new ComparableList<string>()
+            {
+                "a",
+                "b",
+                "d"
+            };
+
+            var l2 = new ComparableList<string>()
+            {
+                "a",
+                "c",
+                "b"
+            };
+
+            Assert.AreNotEqual(l1, l2);
+        }
+
+        [TestMethod]
+        public void TestNullEquality()
+        {
+            ComparableList<string> l1 = null;
+            ComparableList<string> l2 = null;
+
+            var l3 = new ComparableList<string>()
+            {
+                "a",
+                "c",
+                "b"
+            };
+
+            Assert.AreEqual(l1, l2);
+            Assert.AreNotEqual(l1, l3);
+        }
+    }
+}
